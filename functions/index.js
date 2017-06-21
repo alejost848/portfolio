25a07b81fb068ac44794b359d678ce50236cd640
alejost848 <alejost848@gmail.com>
2017-06-21 12:36:35 -0500

    Add a test function for Firebase Cloud Functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,21 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.test = functions.database
+  .ref('/tutorials/{pushId}/thumbnail')
+  .onWrite(event => {
+    // Only edit data when it is first created.
+    if (event.data.previous.exists()) {
+      return;
+    }
+    // Exit when the data is deleted.
+    if (!event.data.exists()) {
+      return;
+    }
+
+    // TODO: Process image and get the dominant color
+    let dominantColor = "OMG it worked!";
+    return event.data.ref.set(dominantColor);
+  });
