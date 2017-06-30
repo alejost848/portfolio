d398b87829e20791049c1633bc52c93e78b8cbf9
alejost848 <alejost848@gmail.com>
2017-06-29 23:39:40 -0500

    Write a test Firebase function

--- functions/index.js
+++ functions/index.js
@@ -4,18 +4,21 @@ const functions = require('firebase-functions');
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.test = functions.database
-  .ref('/tutorials/{pushId}/thumbnail')
-  .onWrite(event => {
-    // Only edit data when it is first created.
-    if (event.data.previous.exists()) {
-      return;
-    }
-    // Exit when the data is deleted.
+  .ref('/tutorials/{tutorialKey}')
+  .onWrite(event => { //Trigger an event when a new tutorial is added
+
+    // If we are deleting the data stop doing stuff
     if (!event.data.exists()) {
+      console.log("Bye, data.");
       return;
     }
 
-    // TODO: Process image and get the dominant color
-    let dominantColor = "OMG it worked!";
-    return event.data.ref.set(dominantColor);
+    // Only edit data when it is first created
+    if (event.data.previous.exists()) {
+      console.log("Hey, data exists already!");
+      return;
+    }
+
+    // const tutorial = event.data.val();
+    // return event.data.ref.update({mainColor: "test"});
   });
