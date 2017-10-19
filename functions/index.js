e492a9d40324af9e86a856bcca47c8128f49c698
Alejandro Sanclemente <alejost848@gmail.com>
2017-10-19 14:26:45 -0500

    Update firebase-functions to 0.5.9 / Add test function

--- functions/index.js
+++ functions/index.js
@@ -1,24 +1,12 @@
 const functions = require('firebase-functions');
+const slugify = require('slugify');
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-
-exports.test = functions.database
+exports.createTutorialSlug = functions.database
   .ref('/tutorials/{tutorialKey}')
-  .onWrite(event => { //Trigger an event when a new tutorial is added
+  .onCreate(event => { //Trigger an event when a new tutorial is added
 
-    // If we are deleting the data stop doing stuff
-    if (!event.data.exists()) {
-      console.log("Bye, data.");
-      return;
-    }
+    console.log("I'm here!");
+    console.log(event.data.val());
 
-    // Only edit data when it is first created
-    if (event.data.previous.exists()) {
-      console.log("Hey, data exists already!");
-      return;
-    }
-
-    // const tutorial = event.data.val();
     // return event.data.ref.update({mainColor: "test"});
   });
