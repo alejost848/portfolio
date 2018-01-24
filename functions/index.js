cbbbc4080d81a42c39987f9bb0d396addde27147
Alejandro Sanclemente <alejost848@gmail.com>
2018-01-23 21:57:41 -0500

    [Functions] Only generate thumbnails for covers

--- functions/index.js
+++ functions/index.js
@@ -176,17 +176,22 @@ exports.handleFormSubmit = functions.https.onRequest((req, res) => {
 exports.generateThumbnail = functions.storage.object().onChange(event => {
   const object = event.data; // The Storage object.
 
-  console.log(object);
-
   const fileBucket = object.bucket; // The Storage bucket that contains the file.
   const filePath = object.name; // File path in the bucket.
   const contentType = object.contentType; // File content type.
   const resourceState = object.resourceState; // The resourceState is 'exists' or 'not_exists' (for file/folder deletions).
-  const metageneration = object.metageneration; // Number of times metadata has been generated. New objects have a value of 1.
 
   // Exit if this is triggered on a file that is not an image.
   if (!contentType.startsWith('image/')) {
     console.log('This is not an image.');
+    return null;
+  }
+
+  // Get the directory name.
+  const directoryName = path.dirname(filePath);
+  // Exit if the image is not a cover.
+  if (!directoryName.endsWith('/cover')) {
+    console.log('This is not a cover.');
     return null;
   }
 
@@ -201,13 +206,6 @@ exports.generateThumbnail = functions.storage.object().onChange(event => {
   // Exit if this is a move or deletion event.
   if (resourceState === 'not_exists') {
     console.log('This is a deletion event.');
-    return null;
-  }
-
-  // Exit if file exists but is not new and is only being triggered
-  // because of a metadata change.
-  if (resourceState === 'exists' && metageneration > 1) {
-    console.log('This is a metadata change event.');
     return null;
   }
 
